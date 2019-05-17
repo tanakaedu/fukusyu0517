cd760edede21af2411a9c80aa5e3c6069652d4f0
tanakaedu <tanaka@am1.jp>
2019-05-17 13:49:40 +0900

    完成

--- fukusyu0517/Form1.Designer.cs
+++ fukusyu0517/Form1.Designer.cs
@@ -29,11 +29,128 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			//
+			// label1
+			//
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(126, 107);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			//
+			// button1
+			//
+			this.button1.Location = new System.Drawing.Point(269, 220);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			//
+			// button2
+			//
+			this.button2.Location = new System.Drawing.Point(223, 249);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			//
+			// button3
+			//
+			this.button3.Location = new System.Drawing.Point(304, 249);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			//
+			// button4
+			//
+			this.button4.Location = new System.Drawing.Point(269, 279);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			//
+			// textBox1
+			//
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 19);
+			this.textBox1.TabIndex = 5;
+			this.textBox1.Text = "0";
+			//
+			// textBox2
+			//
+			this.textBox2.Location = new System.Drawing.Point(118, 12);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 19);
+			this.textBox2.TabIndex = 6;
+			this.textBox2.Text = "0";
+			//
+			// timer1
+			//
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			//
+			// button5
+			//
+			this.button5.Location = new System.Drawing.Point(128, 313);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 7;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			//
+			// Form1
+			//
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(391, 376);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button5;
 	}
 }
 
--- fukusyu0517/Form1.cs
+++ fukusyu0517/Form1.cs
@@ -16,5 +16,41 @@ namespace fukusyu0517
 		{
 			InitializeComponent();
 		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			label1.Left += int.Parse(textBox1.Text);
+			label1.Top += int.Parse(textBox2.Text);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "0";
+			textBox2.Text = "-5";
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "0";
+			textBox2.Text = "5";
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "-5";
+			textBox2.Text = "0";
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "5";
+			textBox2.Text = "0";
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "0";
+			textBox2.Text = "0";
+		}
 	}
 }
